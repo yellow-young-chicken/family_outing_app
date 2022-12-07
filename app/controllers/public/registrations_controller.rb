0c1c01e79b435936387d979b8cf40748c0c0c0b0
EC2 Default User <ec2-user@ip-172-31-45-177.ap-northeast-1.compute.internal>
2022-12-07 17:43:27 +0000

    login update

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
+
+  before_action :configure_permitted_parameters
+
+  def after_sign_up_path_for(resource)
+    flash[:notice] = "登録完了しました"
+    about_path
+  end
+
+  def after_update_path_for(resource)
+    flash[:notice] = "変更完了しました"
+    about_path
+  end
+
+
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -59,4 +73,12 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+
+  # データ操作許可のためのメゾットです。
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:last_name, :first_name, :last_name_kana, :first_name_kana, :account_name, :email, :phone_number])
+  end
+
+
 end
