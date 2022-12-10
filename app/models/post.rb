241ef20b78ac8cdc3dbe10747e8f46f9b4818cca
EC2 Default User <ec2-user@ip-172-31-45-177.ap-northeast-1.compute.internal>
2022-12-10 14:55:11 +0000

    post fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,21 +1,21 @@
 class Post < ApplicationRecord
 
   belongs_to :customer
+  belongs_to :spot
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :post_tags
 
 
-  has_many_attached :images
 
-  def get_images
+  has_many_attached :images
+# 投稿画像がなかった場合の設定、サイズの調整のためのメゾットです。
+  def get_images(width, height)
     unless images.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io:File.open(file_path),filename: 'default-image.jpg', content_type: 'image/jpeg')
-      images
-    else
-      'no_image.jpg'
-  end
+      images.attach(io:File.open(file_path),filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    images.variant(resize_to_limit: [width, height]).processed
 end
 
 
