0ad6b87fe819f2ae6f3b58b7fe1041f85ed31d64
EC2 Default User <ec2-user@ip-172-31-45-177.ap-northeast-1.compute.internal>
2022-12-11 08:19:44 +0000

    bundle

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,4 +1,8 @@
 class Public::CustomersController < ApplicationController
+  
+  before_action :is_matching_login_customer, only: [:edit, :update]
+  
+  
   def new
   end
 
@@ -14,19 +18,29 @@ class Public::CustomersController < ApplicationController
   def edit
     @customer = Customer.find(params[:id])
   end
-  
+
   def update
     @customer = Customer.find(params[:id])
     @customer.update(customer_params)
     redirect_to customer_path(@customer.id)
   end
-  
-  
+
+
   private
-  
+
   def customer_params
-    
     params.require(:customer).permit(:account_name,:profile_image)
+  end
+
+  # ログインしているユーザー本人かどうか確認するためのメゾットです。
+
+  def  is_matching_login_customer
+
+    customer_id = params[:id].to_i
+    login_customer_id = current_customer.id
+    if(customer_id != login_customer_id)
+      redirect_to posts_path
+    end
   end
 
 
