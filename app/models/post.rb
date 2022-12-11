fa37abb73f5ff859b1583f0b2efc4d447c606f2d
EC2 Default User <ec2-user@ip-172-31-45-177.ap-northeast-1.compute.internal>
2022-12-11 11:39:58 +0000

    fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,13 +13,13 @@ class Post < ApplicationRecord
 
 
   has_many_attached :images
-# 投稿画像がなかった場合の設定、サイズの調整のためのメゾットです。
-  def get_images(width, height)
+# 投稿画像がなかった場合のメゾットです。
+  def get_images
     unless images.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       images.attach(io:File.open(file_path),filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    images.variant(resize_to_limit: [width, height]).processed
+    images
   end
 
 def favorited_by?(customer)
