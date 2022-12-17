654a344edcf2603b19ae4d53c0a789d2edcf2723
EC2 Default User <ec2-user@ip-172-31-45-177.ap-northeast-1.compute.internal>
2022-12-17 12:17:37 +0000

    fav

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -4,14 +4,14 @@ class Public::FavoritesController < ApplicationController
     @post = Post.find(params[:post_id])
     @favorite = current_customer.favorites.new(post_id: @post.id)
     @favorite.save
-    redirect_to request.referer
+    render 'replace_btn'
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     @favorite = current_customer.favorites.find_by(post_id: @post.id)
     @favorite.destroy
-    redirect_to request.referer
+    render 'replace_btn'
   end
 
 end
