cb2bc559a64026736dad986c8d9a208559271f04
EC2 Default User <ec2-user@ip-172-31-45-177.ap-northeast-1.compute.internal>
2022-12-20 12:20:20 +0000

    notification

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
 
   belongs_to :customer
   belongs_to :post
+  has_many :notifications, dependent: :destroy
 
   validates :comment, presence: true
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,7 @@ class Post < ApplicationRecord
   has_many :liked_customers, through: :favorites, source: :customer
   has_many :comments, dependent: :destroy
   has_many :post_tags
+  has_many :notifications, dependent: :destroy
 
 
   validates :title, presence:true
@@ -57,6 +58,53 @@ end
         end
       end
     spot_id.blank? ? spots : spots.where(spot_id: spot_id)
+  end
+
+
+  def create_notification_like!(current_customer)
+    # すでに「いいね」されているか検索
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and post_id = ? and action = ? ", current_customer.id, customer_id, id, 'like'])
+    # いいねされていない場合のみ、通知レコードを作成
+    if temp.blank?
+      notification = current_customer.active_notifications.new(
+        post_id: id,
+        visited_id: customer_id,
+        action: 'like'
+      )
+      # 自分の投稿に対するいいねの場合は、通知済みとする
+      if notification.visitor_id == notification.visited_id
+        notification.checked = true
+      end
+      notification.save if notification.valid?
+    end
+  end
+
+
+
+
+     def create_notification_comment!(current_customer, comment_id)
+    # 自分以外にコメントしている人をすべて取得し、全員に通知を送る
+    temp_ids = Comment.select(:user_id).where(post_id: id).where.not(customer_id: current_customer.id).distinct
+    temp_ids.each do |temp_id|
+      save_notification_comment!(current_customer, comment_id, temp_id['user_id'])
+    end
+    # まだ誰もコメントしていない場合は、投稿者に通知を送る
+    save_notification_comment!(current_customer, comment_id, customer_id) if temp_ids.blank?
+  end
+
+  def save_notification_comment!(current_customer, comment_id, visited_id)
+    # コメントは複数回することが考えられるため、１つの投稿に複数回通知する
+    notification = current_customer.active_notifications.new(
+      post_id: id,
+      comment_id: comment_id,
+      visited_id: visited_id,
+      action: 'comment'
+    )
+    # 自分の投稿に対するコメントの場合は、通知済みとする
+    if notification.visitor_id == notification.visited_id
+      notification.checked = true
+    end
+    notification.save if notification.valid?
   end
 
 
