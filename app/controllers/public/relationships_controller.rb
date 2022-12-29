69b0676e79903cb45dde8f89bb48266e6bac2fdd
EC2 Default User <ec2-user@ip-172-31-45-177.ap-northeast-1.compute.internal>
2022-12-29 10:17:54 +0000

    follow

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,16 +1,16 @@
 class Public::RelationshipsController < Public::ApplicationController
 
   def create
-    customer = Customer.find(params[:customer_id])
-    current_customer.follow(customer)
-    customer.create_notification_follow!(current_customer)
-		redirect_to request.referer
+    @customer = Customer.find(params[:customer_id])
+    current_customer.follow(@customer)
+    @customer.create_notification_follow!(current_customer)
+		#redirect_to request.referer
   end
 
   def destroy
-    customer = Customer.find(params[:customer_id])
-    current_customer.unfollow(customer)
-		redirect_to request.referer
+    @customer = Customer.find(params[:customer_id])
+    current_customer.unfollow(@customer)
+		#redirect_to request.referer
   end
 
   def followings
